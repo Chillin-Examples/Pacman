ea58decd9bd3dcae93265dd8a4c2b8b101b426a5
zahra <zahra_9877@yahoo.com>
2018-11-25 17:57:28 +0330

    Remove extra files

--- venv/Lib/site-packages/circuits/net/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Networking Components
-
-This package contains components that implement network sockets and
-protocols for implementing client and server network applications.
-"""
--- venv/Lib/site-packages/circuits/web/websockets/__init__.py
+++
@@ -1,7 +0,0 @@
-"""circuits.web websockets"""
-
-from .client import WebSocketClient
-from .dispatcher import WebSocketsDispatcher
-
-# flake8: noqa
-# pylama: skip=1
