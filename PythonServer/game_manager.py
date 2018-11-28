81fce8d20f5c5b771b1bee408d26bd3fd394d940
zahra <zahra_9877@yahoo.com>
2018-11-29 01:32:36 +0330

    Remove can_move and calclulate_new_pos methods from apply_command to logic_handler

--- PythonServer/game_manager.py
+++ PythonServer/game_manager.py
@@ -47,7 +47,7 @@ class GameManager(RealtimeGameHandler):
     def on_process_cycle(self):
         print('cycle %i' % (self.current_cycle, ))
 
-        self._gui_events = self.logic_handler.process(self.current_cycle)
+        self._gui_events = self._logic_handler.process(self.current_cycle)
 
 
     def on_update_clients(self):
