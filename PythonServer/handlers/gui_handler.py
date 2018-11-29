1d7c440ed8b88c9c4ef5f94eb0dd96ce3e94da47
zahra <zahra_9877@yahoo.com>
2018-11-29 03:57:44 +0330

    Change some attributes of gui_handler to private attributes

--- PythonServer/handlers/gui_handler.py
+++ PythonServer/handlers/gui_handler.py
@@ -36,10 +36,10 @@ class GuiHandler():
 
     def config(self, config):
         
-        self.scale_factor = (self._canvas.width - config['statuses_width']) / (self._world.width * config['cell_size'])
-        self.scale_percent = math.ceil(self.scale_factor * 100)
-        self.cell_size = math.ceil(config['cell_size'] * self.scale_factor)
-        self.font_size = self.cell_size // 2
+        self._scale_factor = (self._canvas.width - config['statuses_width']) / (self._world.width * config['cell_size'])
+        self._scale_percent = math.ceil(self._scale_factor * 100)
+        self._cell_size = math.ceil(config['cell_size'] * self._scale_factor)
+        self._font_size = self._cell_size // 2
 
 
 
@@ -50,32 +50,32 @@ class GuiHandler():
                 cell = board[y][x]
 
                 if cell == ECell.Wall and (y == height - 1 or y == 0 or x == width - 1 or x == 0):
-                    self._canvas.create_image('RoundWall', x * self.cell_size, y * self.cell_size, scale_type=ScaleType.ScaleToWidth,
-                                              scale_value=self.cell_size)
+                    self._canvas.create_image('RoundWall', x * self._cell_size, y * self._cell_size, scale_type=ScaleType.ScaleToWidth,
+                                              scale_value=self._cell_size)
 
                 elif cell == ECell.Wall:
-                    self._canvas.create_image('InterWall', x * self.cell_size, y * self.cell_size, scale_type=ScaleType.ScaleToWidth,
-                                              scale_value=self.cell_size)
+                    self._canvas.create_image('InterWall', x * self._cell_size, y * self._cell_size, scale_type=ScaleType.ScaleToWidth,
+                                              scale_value=self._cell_size)
 
                 elif cell == ECell.Empty:
-                    self._canvas.create_image('Empty', x * self.cell_size, y * self.cell_size, scale_type=ScaleType.ScaleToWidth,
-                                              scale_value=self.cell_size)
+                    self._canvas.create_image('Empty', x * self._cell_size, y * self._cell_size, scale_type=ScaleType.ScaleToWidth,
+                                              scale_value=self._cell_size)
 
                 elif cell == ECell.Food:
-                    img_ref = self._canvas.create_image('Food', x * self.cell_size, y * self.cell_size, scale_type=ScaleType.ScaleToWidth,
-                                              scale_value=self.cell_size)
+                    img_ref = self._canvas.create_image('Food', x * self._cell_size, y * self._cell_size, scale_type=ScaleType.ScaleToWidth,
+                                              scale_value=self._cell_size)
 
 
                 elif cell == ECell.SuperFood:
-                    self._canvas.create_image('SuperFood', x * self.cell_size, y * self.cell_size, scale_type=ScaleType.ScaleToWidth,
-                                              scale_value=self.cell_size)
+                    self._canvas.create_image('SuperFood', x * self._cell_size, y * self._cell_size, scale_type=ScaleType.ScaleToWidth,
+                                              scale_value=self._cell_size)
 
 
     def draw_players(self, height, width, board):
 
-        self.pacman_img_ref = self._canvas.create_image('Pacman', self._world.pacman.x, self._world.pacman.y, center_origin=True,
+        self._pacman_img_ref = self._canvas.create_image('Pacman', self._world.pacman.x, self._world.pacman.y, center_origin=True,
                                 scale_type=ScaleType.ScaleToWidth,
-                                scale_value=self.cell_size)
+                                scale_value=self._cell_size)
 
 
     def update(self, events):
@@ -83,4 +83,4 @@ class GuiHandler():
         for event in events:
             if event.type == GuiEventType.MovePacman:
                 pacman_dir = self.pacman_angle[event.extra_properties["direction"]]
-                self._canvas.edit_image(self.pacman_img_ref, event.extra_properties["new_pos"][0], event.extra_properties["new_pos"][1], angle=pacman_dir)
+                self._canvas.edit_image(self._pacman_img_ref, event.extra_properties["new_pos"][0], event.extra_properties["new_pos"][1], angle=pacman_dir)
