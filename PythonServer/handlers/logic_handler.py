e1c52eb3b802f8b979ee918b5b346287b1269792
zahra <zahra_9877@yahoo.com>
2018-12-01 02:03:58 +0330

    fix: Remove direction from parameters of GuiEvent when type is MovePacman

--- PythonServer/handlers/logic_handler.py
+++ PythonServer/handlers/logic_handler.py
@@ -63,7 +63,7 @@ class LogicHandler ():
 
                 self.world.pacman.x = new_position[0]
                 self.world.pacman.y = new_position[1]
-                return [GuiEvent(GuiEventType.MovePacman, new_pos=new_position, direction=self.world.pacman.direction.name)]
+                return [GuiEvent(GuiEventType.MovePacman, new_pos=new_position)]
 
             else:
                 print("cannot move")
