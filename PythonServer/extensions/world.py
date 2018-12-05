56d175112e817d1a6c212276587fb64e6cd8e0a8
zahra <zahra_9877@yahoo.com>
2018-12-05 10:46:13 +0330

    fix: Fix a minor error

--- PythonServer/extensions/world.py
+++ PythonServer/extensions/world.py
@@ -9,6 +9,7 @@ from gui_events import GuiEvent, GuiEventType
 def apply_command(self, side_name, command):
 
     if command.name() == ChangePacmanDirection.name():
+        # if self._can_change_direction(command)
         self.pacman.change_direction(command)
         return [GuiEvent(GuiEventType.ChangePacmanDirection, direction=self.pacman.direction)]
 
@@ -16,4 +17,9 @@ def apply_command(self, side_name, command):
         self.ghosts[command.id].change_direction(command)
         return [GuiEvent(GuiEventType.ChangeGhostDirection, id=command.id, direction=self.ghosts[command.id].direction)]
 
+
+# def _can_change_direction(self, command):
+#     print (command.name())
+
 World.apply_command = apply_command
+# World._can_change_direction = _can_change_direction
