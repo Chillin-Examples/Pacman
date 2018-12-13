a5cbbc6bf44b0e930f38a9c54bd94a800257ec94
ghazalb76 <ghazal.b93@gmail.com>
2018-12-13 22:20:17 +0330

    feat: Add class for status bar

---
+++ PythonServer/handlers/game_status.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# python imports
+
+# chillin imports
+
+# project imports
+
+
+class Game_status():
+
+    def __init__():
+        pass
+    
+    def initialize(self):
+        pass
+
+    def draw_statuses(self):
+        pass
+
+    def update_statuses(self):
+        pass
