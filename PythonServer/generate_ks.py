71c5f6f4567d8c54f1d6b0f0a31c73f5518b9c7b
zahra <zahra_9877@yahoo.com>
2018-12-26 08:02:39 +0330

    ref: Chnage the serializer version and some changes in generate_ks

--- PythonServer/generate_ks.py
+++ PythonServer/generate_ks.py
@@ -8,5 +8,9 @@ lang = 'python'
 if len(sys.argv) > 1:
     lang = sys.argv[1]
 
+all_args = [('python', 'app/ks', 'snake_case'), ('python', '../PythonClient/ks', 'snake_case'),
+            ('python', '../PythonRandomClient/ks', 'snake_case'),
+            ('cpp', '../CppClient/Game/ks', 'camelCase')]
+
 generate('ks/commands.ks', lang, 'ks')
 generate('ks/models.ks', lang, 'ks')
